2a6bcd48117622536c35aa540b193680de13014f
Manaure Vasconcelos <manaurevasc97@gmail.com>
2025-03-12 11:34:51 -0300

    refactor: restructure project directories and configs

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,8 @@
-import fastify from 'fastify'
+import fastify, { FastifyInstance } from 'fastify'
 
-const server = fastify()
-
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
+const server: FastifyInstance = fastify({
+  logger: true
+});
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
